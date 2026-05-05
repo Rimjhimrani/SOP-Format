67c4d4840eb53ecc556b04a5b947cb38ce0e4013
Rimjhimrani <144531476+Rimjhimrani@users.noreply.github.com>
2026-05-05 11:46:25 +0530

    Update sop.py

--- sop.py
+++ sop.py
@@ -639,7 +639,7 @@ def generate_pdf(steps, meta):
     title_mid = (hdr_top + hdr_bot) / 2          # vertical center of header block
     c.setFont("Helvetica-Bold", 13); c.setFillColor(colors.black)
     c.drawCentredString(title_cx, title_mid + 6, "STANDARD OPERATING PROCEDURE")
-    sub_lines = wrapped_lines_pdf(c, meta["title"], TITLE_W-8, "Helvetica", 10)
+    sub_lines = wrapped_lines_pdf(c, meta["title"], TITLE_W-8, "Helvetica", 13)
     c.setFont("Helvetica", 10)
     for i, ln in enumerate(sub_lines):
         c.drawCentredString(title_cx, title_mid - 8 - i*12, ln)
